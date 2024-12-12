ba1f05ae652cefb2e4bf1722de19fc70a1d2802e
JaimeMarti <jaimarher@alu.edu.gva.es>
2024-12-12 20:25:16 +0100

    First commit

---
+++ android/app/src/main/kotlin/com/example/u4_pa1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.u4_pa1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/domain/repositories/character_repository.dart
@@ -0,0 +1,6 @@
+import 'package:dartz/dartz.dart';
+import 'package:u4_pa1/domain/entities/character.dart';
+
+abstract class CharacterRepository {
+  Future<Either<Exception, List<Character>>> getAllCharacters();
+}
